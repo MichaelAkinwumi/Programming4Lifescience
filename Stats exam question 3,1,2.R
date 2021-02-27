dfb51873ea72e3309e19752c874336b04b046d37
MichaelAkinwumi <78554464+MichaelAkinwumi@users.noreply.github.com>
2021-02-27 16:06:59 +0100

    Add files via upload

---
+++ Stats exam question 3,1,2.R
@@ -0,0 +1,23 @@
+
+Question1
+#assigning variables
+Sample<-500
+Statistic<-"42%"
+
+Parameter= The percentage
+Population = Thevotersinthestate
+
+
+
+
+Question3
+#vectors
+vec1<-c( 32.2, 37.5, 41.7, 44.8, 46.3, 48.2, 50.2, 53.8, 65.0)
+vec1
+mean(vec1)
+median(vec1)
+
+vec2<-c(32.2, 37.5, 41.7, 44.8, 46.3, 46.5, 48.2, 50.2, 53.8)
+vec2
+mean(vec2)
+median(vec2)
